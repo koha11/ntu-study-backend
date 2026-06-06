f212dad2b8d86f48e7097648369bee49c8e817b8
koha11 <khoa88108@gmail.com>
2026-06-06 21:50:13 +0700

    remove log on health api

--- src/modules/health/health.service.ts
+++ src/modules/health/health.service.ts
@@ -4,7 +4,6 @@ import { Injectable, Logger } from '@nestjs/common';
 export class HealthService {
   private readonly logger = new Logger(HealthService.name);
   getHealth() {
-    this.logger.debug('Health check requested');
     return {
       status: 'ok',
       timestamp: new Date().toISOString(),
